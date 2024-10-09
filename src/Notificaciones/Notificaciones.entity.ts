af36df088ff2ba87f21be103db82e5b8d3823859
Android Studio <devandroid@ort>
2024-10-09 10:03:04 -0300

    aaaa

--- src/Notificaciones/Notificaciones.entity.ts
+++ src/Notificaciones/Notificaciones.entity.ts
@@ -1,14 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 export enum Tipo {
-  tipo1 = 'Invitacion_Partido', // No van a haber invitaciones
-  tipo2 = 'Invitacion_Torneo', //Cuando te invito para entrar a un torneo
-  tipo3 = 'Global',
-  tipo4 = 'Confirmar_Resultado_Partido',
-  tipo5 = 'Confirmar_Resultado_Partido_Torneo',
-  tipo6 = 'Confirmar_Partido_Torneo', // el partido ya dentro del torneo
-  tipo7 = 'Notificaciones',
-  tipo8 = 'Sancion',
+  tipo1 = 'Invitacion_Partido', // eliminar
+  tipo2 = 'Invitacion_Torneo', // eliminar
+  tipo3 = 'Global', // notificaciones para todo el mundo sobre actualizaciones y similares
+  tipo4 = 'Confirmar_Resultado_Partido', // confirmar el resultado del partido
+  tipo5 = 'Confirmar_Resultado_Partido_Torneo', // confirmar el resultado del partido de un torneo
+  tipo6 = 'Confirmar_Partido_Torneo', // eliminar
+  tipo7 = 'Notificacion', // aviso previo a sancion
+  tipo8 = 'Sancion', // una sancion por mal comportamiento
 }
 
 @Entity()
